81f78b86694c1168e26d35e9c240007c7c9ba954
Fabio Prizio <fabio.prizio@casavo.com>
2024-06-07 02:07:26 +0200

    feat(endpoint-2): implement HttpTranslationService for Yoda and Shakespeare

--- src/main/kotlin/com/pokedex/infrastructure/translation/HttpTranslationService.kt
+++ src/main/kotlin/com/pokedex/infrastructure/translation/HttpTranslationService.kt
@@ -1,3 +1,55 @@
 package com.pokedex.infrastructure.translation
 
-class HttpTranslationService {}
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
+import com.pokedex.domain.translation.TranslationService
+import com.pokedex.infrastructure.installRetryConfiguration
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.client.request.*
+import io.ktor.serialization.jackson.*
+import kotlinx.coroutines.runBlocking
+
+class HttpTranslationService(translationBaseUrl: String) : TranslationService {
+    private val httpClient: HttpClient = HttpClient {
+        expectSuccess = false
+        followRedirects = false
+
+        installRetryConfiguration()
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.INFO
+        }
+
+        install(ContentNegotiation) { jackson { configure(FAIL_ON_UNKNOWN_PROPERTIES, false) } }
+
+        install(DefaultRequest)
+        defaultRequest { url(translationBaseUrl) }
+    }
+
+    override fun getYodaTranslation(text: String): String = runBlocking {
+        return@runBlocking getTranslationFor(text, "yoda")
+    }
+
+    override fun getShakespeareTranslation(text: String): String = runBlocking {
+        return@runBlocking getTranslationFor(text, "shakespeare")
+    }
+
+    private suspend fun getTranslationFor(text: String, character: String): String {
+        val response =
+            httpClient
+                .get("translate/$character") { url { parameters.append("text", text) } }
+                .body<TranslationResponse>()
+
+        return response.contents.translated
+    }
+
+    private data class TranslationResponse(
+        @JsonProperty("contents") val contents: TranslationContents
+    )
+
+    private data class TranslationContents(@JsonProperty("translated") val translated: String)
+}
--- src/test/kotlin/com/pokedex/infrastructure/translation/HttpTranslationServiceIntegrationTest.kt
+++ src/test/kotlin/com/pokedex/infrastructure/translation/HttpTranslationServiceIntegrationTest.kt
@@ -1,18 +1,28 @@
 package com.pokedex.infrastructure.translation
 
+import kotlin.test.assertEquals
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class HttpTranslationServiceIntegrationTest {
-    private val service = HttpTranslationService()
+    private val service =
+        HttpTranslationService(translationBaseUrl = "https://api.funtranslations.com")
 
     @Test
     fun `get yoda translation for a string`() = runTest {
-        TODO("Not yet implemented.")
+        val originalString =
+            "It was created by a scientist after years of horrific gene splicing and DNA engineering experiments."
+        val translatedString =
+            "Created by a scientist after years of horrific gene splicing and dna engineering experiments,  it was."
+        assertEquals(translatedString, service.getYodaTranslation(originalString))
     }
 
     @Test
     fun `get shakespeare translation for a string`() = runTest {
-        TODO("Not yet implemented.")
+        val originalString =
+            "When several of these POKéMON gather, their electricity could build and cause lightning storms."
+        val translatedString =
+            "At which hour several of these pokémon gather,  their electricity couldst buildeth and cause lightning storms."
+        assertEquals(translatedString, service.getShakespeareTranslation(originalString))
     }
 }
